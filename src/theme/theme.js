14f2dc466e8b4c58bf44959830649b35b010082d
daylonblakely <daylonblakely@gmail.com>
2021-12-16 10:56:32 -0800

    upated theme

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,4 +1,4 @@
-import { extendTheme } from 'native-base';
+import { extendTheme, themeTools } from 'native-base';
 
 export default theme = extendTheme({
   components: {
@@ -8,41 +8,15 @@ export default theme = extendTheme({
       },
     },
     Box: {
-      defaultProps: {
-        _dark: {
-          borderColor: 'gray.800',
-          backgroundColor: 'gray.900',
-        },
-        borderColor: 'coolGray.200',
-        backgroundColor: 'gray.50',
+      baseStyle: (props) => {
+        return {
+          backgroundColor: themeTools.mode('gray.50', 'gray.900')(props),
+          borderColor: themeTools.mode('coolGray.200', 'gray.800')(props),
+        };
       },
     },
   },
   colors: {
-    // primary: {
-    //   50: '#d6fcee',
-    //   100: '#97f5d4',
-    //   200: '#02eeb9', //dark mode
-    //   300: '#00e49e',
-    //   400: '#00d98a',
-    //   500: '#00ce76',
-    //   600: '#00bd6a',
-    //   700: '#00aa5c',
-    //   800: '#00984e',
-    //   900: '#007636',
-    // },
-    // secondary: {
-    //   50: '#e1f5f9',
-    //   100: '#b4e4ef',
-    //   200: '#86d2e6',
-    //   300: '#5cc1dd',
-    //   400: '#41b4d8',
-    //   500: '#2aa7d4',
-    //   600: '#2299c7', //
-    //   700: '#1687b4',
-    //   800: '#1576a1',
-    //   900: '#095780',
-    // },
     background: {
       dark: '#121212',
       light: '#FFFFFF',
