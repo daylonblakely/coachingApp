3aa31088b0980e3d48c7a3b53bf18bebe2c87e02
daylonblakely <daylonblakely@gmail.com>
2022-10-17 19:58:56 -0700

    update play menu animation

--- src/components/PlayMenu/MenuIcon.js
+++ src/components/PlayMenu/MenuIcon.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { IconButton, VStack, Text } from 'native-base';
 import { Ionicons } from '@expo/vector-icons';
 
-const MenuIcon = ({ bg, icon }) => {
+const MenuIcon = ({ bg, icon, onPress, text }) => {
   return (
     <VStack alignItems="center">
       <IconButton
@@ -12,11 +12,14 @@ const MenuIcon = ({ bg, icon }) => {
         size={12}
         colorScheme={bg.split('.')[0]} //color when pressed
         borderRadius="full"
+        onPress={onPress}
         icon={<Ionicons name={icon} size={26} color="white" />}
       />
-      <Text color="white" mb="3">
-        Icon text
-      </Text>
+      {text && (
+        <Text color="white" mb="3">
+          {text}
+        </Text>
+      )}
     </VStack>
   );
 };
--- src/components/PlayMenu/index.js
+++ src/components/PlayMenu/index.js
@@ -1,13 +1,5 @@
 import React from 'react';
-import {
-  Box,
-  useDisclose,
-  IconButton,
-  Stagger,
-  Icon,
-  Modal,
-} from 'native-base';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Box, useDisclose, Stagger, Modal } from 'native-base';
 import MenuIcon from './MenuIcon';
 
 const PlayMenu = () => {
@@ -21,27 +13,13 @@ const PlayMenu = () => {
           initial={{
             opacity: 0,
             scale: 0,
-            translateY: 34,
           }}
           animate={{
-            translateY: 0,
             scale: 1,
             opacity: 1,
             transition: {
               type: 'spring',
-              mass: 0.8,
-              stagger: {
-                offset: 30,
-                reverse: true,
-              },
-            },
-          }}
-          exit={{
-            translateY: 34,
-            scale: 0.5,
-            opacity: 0,
-            transition: {
-              duration: 100,
+              mass: 1,
               stagger: {
                 offset: 30,
                 reverse: true,
@@ -58,24 +36,7 @@ const PlayMenu = () => {
           <MenuIcon bg="red.400" icon="play-back" />
         </Stagger>
       </Modal>
-      <IconButton
-        variant="solid"
-        borderRadius="full"
-        size={12}
-        onPress={onToggle}
-        bg="cyan.600"
-        icon={
-          <Icon
-            as={MaterialCommunityIcons}
-            size="2xl"
-            name="dots-horizontal"
-            color="warmGray.50"
-            _dark={{
-              color: 'warmGray.50',
-            }}
-          />
-        }
-      />
+      <MenuIcon bg="cyan.600" icon="ellipsis-horizontal" onPress={onToggle} />
     </Box>
   );
 };
