a711c9a1eaa7ea891f3e83c638789adcdfde3c24
daylonblakely <daylonblakely@gmail.com>
2022-10-23 17:53:47 -0700

    useAnimatedReaction with progress for player animation

--- src/hooks/usePlayerAnimation.js
+++ src/hooks/usePlayerAnimation.js
@@ -1,20 +1,43 @@
-import { useContext, useEffect } from 'react';
-// import { Context as PlayContext } from '../context/PlayContext';
+import { useEffect } from 'react';
+import {
+  useSharedValue,
+  useAnimatedReaction,
+  withTiming,
+  runOnJS,
+} from 'react-native-reanimated';
 
 export default (playerPos, shouldAnimate, callback) => {
-  //   const {
-  //     state: { isAnimating },
-  //     stopAnimating,
-  //   } = useContext(PlayContext);
+  const shouldAnimateShared = useSharedValue(false);
+  const progress = useSharedValue(0);
+
+  useAnimatedReaction(
+    () => {
+      return progress.value;
+    },
+    (result) => {
+      if (shouldAnimateShared.value)
+        playerPos.value = { x: playerPos.value.x + 1, y: playerPos.value.y };
+    }
+  );
 
   useEffect(() => {
+    shouldAnimateShared.value = shouldAnimate;
     if (shouldAnimate) {
       console.log('start animation');
-
-      setTimeout(() => {
-        callback();
-        console.log('end animation');
-      }, 5000);
+      progress.value = withTiming(
+        progress.value + 100,
+        { duration: 2000 },
+        (finished) => {
+          if (finished) {
+            console.log('ANIMATION ENDED');
+            // progress.value = playerPos.value.x;
+            // callback();
+            runOnJS(callback)();
+          } else {
+            console.log('ANIMATION GOT CANCELLED');
+          }
+        }
+      );
     }
   }, [shouldAnimate]);
 };
