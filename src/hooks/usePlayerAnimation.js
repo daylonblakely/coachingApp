fbe5a0247b05a662b5748737682fa9370f06a14d
daylonblakely <daylonblakely@gmail.com>
2022-10-26 20:37:05 -0700

    animate players along path based on length

--- src/hooks/usePlayerAnimation.js
+++ src/hooks/usePlayerAnimation.js
@@ -5,42 +5,60 @@ import {
   withTiming,
   runOnJS,
 } from 'react-native-reanimated';
-import { getYForX } from 'react-native-redash';
+import * as path from 'svg-path-properties';
+import { serialize } from 'react-native-redash';
 import { Context as PlayerContext } from '../context/PlayerContext';
+
+const ANIMATION_DURATION = 2000;
 
 export default (playerId, playerPos, shouldAnimate, callback) => {
   const { state } = useContext(PlayerContext);
-  const path = state[playerId].currentPathToNextPos;
+  const currentPath = state[playerId].currentPathToNextPos;
   const shouldAnimateShared = useSharedValue(false);
   const progress = useSharedValue(0);
 
+  const properties =
+    currentPath.move && path.svgPathProperties(serialize(currentPath));
+  const totalLength = properties?.getTotalLength();
+  const pointsAtLength = Array(Math.floor(totalLength + 1 || 0)) // + 1 to avoid destructuring undefined in animation hook
+    .fill()
+    .map((_, i) => properties.getPointAtLength(i));
+
   useAnimatedReaction(
     () => {
-      return progress.value;
+      return Math.floor(progress.value);
     },
     (result) => {
-      if (shouldAnimateShared.value)
+      if (shouldAnimateShared.value) {
+        const { x, y } = pointsAtLength[result];
         playerPos.value = {
-          x: playerPos.value.x + 1,
-          y: getYForX(path, playerPos.value.x + 1),
+          x,
+          y,
         };
-    }
+      }
+    },
+    [pointsAtLength]
   );
 
   useEffect(() => {
     shouldAnimateShared.value = shouldAnimate;
     if (shouldAnimate) {
-      console.log('start animation');
-      progress.value = withTiming(100, { duration: 2000 }, (finished) => {
-        if (finished) {
-          console.log('ANIMATION ENDED');
-          shouldAnimateShared.value = false;
-          progress.value = 0;
-          runOnJS(callback)();
-        } else {
-          console.log('ANIMATION GOT CANCELLED');
+      console.log('START ANIMATION');
+
+      progress.value = withTiming(
+        totalLength,
+        { duration: ANIMATION_DURATION },
+        (finished) => {
+          if (finished) {
+            console.log('ANIMATION ENDED');
+            shouldAnimateShared.value = false;
+            progress.value = 0;
+            runOnJS(callback)();
+          } else {
+            console.log('ANIMATION CANCELLED');
+          }
         }
-      });
+      );
     }
   }, [shouldAnimate]);
 };
