010909976cc5938faf1b90f4f6fa62670e194b37
daylonblakely <daylonblakely@gmail.com>
2023-03-29 21:45:12 -0700

    added draggable dribble path

--- src/hooks/useArrowPoints.js
+++ src/hooks/useArrowPoints.js
@@ -1,7 +1,13 @@
 import { useContext } from 'react';
-import { useAnimatedProps } from 'react-native-reanimated';
+import {
+  runOnJS,
+  useSharedValue,
+  useAnimatedProps,
+  useAnimatedReaction,
+} from 'react-native-reanimated';
 import { serialize } from 'react-native-redash';
 import { getPath } from '../utils/pathUtils';
+import squiggleLine from '../utils/squiggleLine';
 import { Context as PlayContext } from '../context/PlayContext';
 
 export default (playerPos, posMid, posEnd, pathToNextPos) => {
@@ -9,12 +15,25 @@ export default (playerPos, posMid, posEnd, pathToNextPos) => {
     state: { isEditMode },
   } = useContext(PlayContext);
 
+  const dribblePath = useSharedValue('');
+
+  const squiggleLineWrapper = (path) => {
+    dribblePath.value = squiggleLine(path);
+  };
+
+  useAnimatedReaction(
+    () => getPath(playerPos.value, posMid.value, posEnd.value),
+    (result) => {
+      runOnJS(squiggleLineWrapper)(serialize(result));
+    }
+  );
+
   // moves arrow svg
   const animatedPropsArrow = useAnimatedProps(() => {
     const p = isEditMode
       ? getPath(playerPos.value, posMid.value, posEnd.value)
       : pathToNextPos;
-
+    return { d: dribblePath.value };
     return { d: p ? serialize(p) : '' };
   }, [isEditMode, pathToNextPos]);
 
